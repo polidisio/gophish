e748a9a0d79348d17e9ffe0d2954d51d1ee9e9b3
polidisio <polidisio@users.noreply.github.com>
2026-03-03 17:18:42 +0100

    fix: add more debug logging to analytics

--- static/js/src/app/analytics.js
+++ static/js/src/app/analytics.js
@@ -17,40 +17,51 @@ var Dashboard = {
     },
     
     load: function() {
-        console.log("Loading analytics...");
-        console.log("User API key:", user ? user.api_key : "NOT SET");
+        var self = this;
+        console.log("Loading analytics data...");
+        
+        function waitForApi() {
+            if (typeof api !== 'undefined' && api.analytics) {
+                self.fetchData();
+            } else {
+                console.log("Waiting for api...");
+                setTimeout(waitForApi, 100);
+            }
+        }
+        waitForApi();
+    },
+    
+    fetchData: function() {
+        var self = this;
         $("#loading").show();
         $("#dashboard-content").hide();
         
         api.analytics.summary().then(function(response) {
             console.log("Summary response:", response);
-            Dashboard.renderSummary(response);
+            self.renderSummary(response);
             $("#loading").hide();
             $("#dashboard-content").show();
         }).catch(function(err) {
             console.error("Error loading summary:", err);
-            $("#loading").hide();
-            $("#dashboard-content").show();
-            $("#dashboard-content").html("<div class='alert alert-warning'>Please login to view analytics</div>");
         });
         
         api.analytics.departments().then(function(response) {
             console.log("Departments response:", response);
-            Dashboard.renderDepartments(response);
+            self.renderDepartments(response);
         }).catch(function(err) {
             console.error("Error loading departments:", err);
         });
         
         api.analytics.userScores().then(function(response) {
             console.log("User scores response:", response);
-            Dashboard.renderUserScores(response);
+            self.renderUserScores(response);
         }).catch(function(err) {
             console.error("Error loading user scores:", err);
         });
         
         api.analytics.campaigns().then(function(response) {
             console.log("Campaigns response:", response);
-            Dashboard.renderCampaigns(response);
+            self.renderCampaigns(response);
         }).catch(function(err) {
             console.error("Error loading campaigns:", err);
         });
